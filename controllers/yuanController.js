f50a222193fa3fc40696daa9c1ac1bb4f0dc945c
Anastasia Batyrova <batyrova.n@icloud.com>
2023-09-27 21:28:58 +0300

    Yuan

--- controllers/yuanController.js
+++ controllers/yuanController.js
@@ -3,7 +3,8 @@ const {Yuan} = require('../models/models')
 class YuanController {
     async getCourse(req, res) {
         const course = await Yuan.findAll({attributes: ['id', 'course']})
-        return res.json(course)
+        if (course)
+        return res.json(course[0])
     }
 }
 
