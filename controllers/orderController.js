4ec2bde970907dc9a651b894926388f948cc0909
Anastasia Batyrova <batyrova.n@icloud.com>
2023-11-21 22:52:28 +0300

    new db

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,12 +1,25 @@
-const {Order} = require('../models/models')
+const { Order } = require('../models/models')
 
 class OrderController {
     async getOrder(req, res) {
-        const {track} = req.query
+        const { track } = req.query
         if (track) {
-            const order = await Order.findOne({where: {track}, attributes: ['id', 'track', 'status']})
+            const order = await Order.findOne({ where: { track }, attributes: ['id', 'track', 'status'] })
             return res.json(order)
         } else {
+            return res.json(null)
+        }
+    }
+
+    async copyOrders(req, res) {
+        const { orders } = req.body
+        try {
+            for (let i of orders) {
+                await Order.create({ track: i.track, status: i.status })
+            }
+            return res.json(orders)
+        } catch (e) {
+            console.log(e)
             return res.json(null)
         }
     }
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,5 +3,6 @@ const orderController = require('../controllers/orderController')
 const router = new Router()
 
 router.get('/', orderController.getOrder)
+router.post('/', orderController.copyOrders)
 
 module.exports = router
