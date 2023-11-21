f4c1011628cbe35b3299e65bd1d70ff4f0f38f7c
Anastasia Batyrova <batyrova.n@icloud.com>
2023-11-21 23:16:46 +0300

    https settings

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
 const path = require('path')
+const https = require('https')
+const fs = require('fs')
 
 const PORT = process.env.PORT || 5000
+
+// for linux
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/wearpoizon.workinit.ru/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/wearpoizon.workinit.ru/cert.pem')
+}
 
 const app = express()
 app.use(cors())
@@ -14,11 +23,18 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
 
+// for linux
+
+const server = https.createServer(options, app)
+
 const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        // app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        // for linux
+
+        server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
