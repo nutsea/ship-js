690bbe75ba7fdceff1aabc212773711a48b289ba
nutsea <batyrova.n@icloud.com>
2025-01-31 22:37:32 +0300

    new route

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
-app.use('/api', router)
+app.use('/bot', router)
 
 // for linux
 
